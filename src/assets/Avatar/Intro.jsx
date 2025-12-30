17b4cce5d725b81e848affd6c01292ba9a0a342d
Hanem Naga <hanem.naga1@gmail.com>
2025-12-30 18:13:57 +0200

    do a simple change

--- src/assets/Avatar/Intro.jsx
+++ src/assets/Avatar/Intro.jsx
@@ -1,7 +1,7 @@
 export default function Intro() {
   return (
     <div className="intro">
-      <h2>the genius developer</h2>
+      <h2>the good developer</h2>
       <p>
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus
         tenetur eaque velit modi corrupti debitis magnam vel aspernatur repellat
